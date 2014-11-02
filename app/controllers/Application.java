74333e45e5b857c7eb64a63769016e91bb7c7f44
Santhosh Kattissery <santhosh.kattissery@compassitesinc.com>
2014-11-02 13:24:49 +0530

    Amadeus flight info conrtoller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 
+import com.compassites.model.FlightItinerary;
 import com.compassites.model.PNRResponse;
 import com.compassites.model.SearchParameters;
 import com.compassites.model.SearchResponse;
@@ -12,6 +13,7 @@ import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 import services.BookingServiceWrapper;
+import services.FlightInfoServiceWrapper;
 import services.FlightSearchWrapper;
 
 import java.util.List;
@@ -27,6 +29,9 @@ public class Application {
 
     @Autowired
     private BookingServiceWrapper bookingService;
+    
+    @Autowired
+    private FlightInfoServiceWrapper flightInfoService;
 
     public Result flightSearch(){
         System.out.println("Request recieved");
@@ -38,9 +43,7 @@ public class Application {
         return Controller.ok(Json.toJson(searchParameters.redisKey()));
     }
 
-
     public Result generatePNR(){
-
         JsonNode json = request().body().asJson();
 
         TravellerMasterInfo travellerMasterInfo = Json.fromJson(json, TravellerMasterInfo.class);
@@ -51,7 +54,6 @@ public class Application {
 
     @BodyParser.Of(BodyParser.Json.class)
     public Result priceChangePNR(){
-
         JsonNode json = request().body().asJson();
 
         TravellerMasterInfo travellerMasterInfo = Json.fromJson(json, TravellerMasterInfo.class);
@@ -59,7 +61,15 @@ public class Application {
         System.out.println("-----------------PNR Response: " + Json.toJson(pnrResponse));
         return Controller.ok(Json.toJson(pnrResponse));
     }
-
+    
+    @BodyParser.Of(BodyParser.Json.class)
+    public Result getFlightInfo() {
+    	JsonNode json = request().body().asJson();
+    	SearchParameters searchParams = Json.fromJson(json.findPath("searchParams"), SearchParameters.class);
+    	FlightItinerary flightItinerary = Json.fromJson(json.findPath("flightItinerary"), FlightItinerary.class);
+    	FlightItinerary response = flightInfoService.getFlightInfo(flightItinerary, searchParams);
+    	return Controller.ok(Json.toJson(response));
+    }
 
     public Result issueTicket(String gdsPNR){
         PNRResponse pnrResponse = bookingService.issueTicket(gdsPNR);
