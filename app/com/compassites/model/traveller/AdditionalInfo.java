bb1fe97455e7938213a21aac7878e6e31a76f1b8
Santhosh Kattissery <santhosh.kattissery@compassitesinc.com>
2014-11-12 11:05:12 +0530

    Json ignore some properties

--- app/com/compassites/model/traveller/AdditionalInfo.java
+++ app/com/compassites/model/traveller/AdditionalInfo.java
@@ -5,103 +5,103 @@ package com.compassites.model.traveller;
  */
 public class AdditionalInfo {
 
-    private Integer Id;
+	private Integer id;
 
-    private String phoneNumber;
+	private String phoneNumber;
 
-    private String email;
+	private String email;
 
-    private String emergencyContact;
+	private String emergencyContact;
 
-    private String vesselName;
+	private String vesselName;
 
-    private String vesselRegistration;
+	private String vesselRegistration;
 
-    private String purposeOfTravel;
+	private String purposeOfTravel;
 
-    private String emergencyContactNumber;
+	private String emergencyContactNumber;
 
-    private String countryCode;
+	private String countryCode;
 
-    private String   phoneCode;
+	private String phoneCode;
 
-    public Integer getId() {
-        return Id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        Id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getEmergencyContact() {
-        return emergencyContact;
-    }
+	public String getEmergencyContact() {
+		return emergencyContact;
+	}
 
-    public void setEmergencyContact(String emergencyContact) {
-        this.emergencyContact = emergencyContact;
-    }
+	public void setEmergencyContact(String emergencyContact) {
+		this.emergencyContact = emergencyContact;
+	}
 
-    public String getVesselName() {
-        return vesselName;
-    }
+	public String getVesselName() {
+		return vesselName;
+	}
 
-    public void setVesselName(String vesselName) {
-        this.vesselName = vesselName;
-    }
+	public void setVesselName(String vesselName) {
+		this.vesselName = vesselName;
+	}
 
-    public String getVesselRegistration() {
-        return vesselRegistration;
-    }
+	public String getVesselRegistration() {
+		return vesselRegistration;
+	}
 
-    public void setVesselRegistration(String vesselRegistration) {
-        this.vesselRegistration = vesselRegistration;
-    }
+	public void setVesselRegistration(String vesselRegistration) {
+		this.vesselRegistration = vesselRegistration;
+	}
 
-    public String getPurposeOfTravel() {
-        return purposeOfTravel;
-    }
+	public String getPurposeOfTravel() {
+		return purposeOfTravel;
+	}
 
-    public void setPurposeOfTravel(String purposeOfTravel) {
-        this.purposeOfTravel = purposeOfTravel;
-    }
+	public void setPurposeOfTravel(String purposeOfTravel) {
+		this.purposeOfTravel = purposeOfTravel;
+	}
 
-    public String getEmergencyContactNumber() {
-        return emergencyContactNumber;
-    }
+	public String getEmergencyContactNumber() {
+		return emergencyContactNumber;
+	}
 
-    public void setEmergencyContactNumber(String emergencyContactNumber) {
-        this.emergencyContactNumber = emergencyContactNumber;
-    }
+	public void setEmergencyContactNumber(String emergencyContactNumber) {
+		this.emergencyContactNumber = emergencyContactNumber;
+	}
 
-    public String getCountryCode() {
-        return countryCode;
-    }
+	public String getCountryCode() {
+		return countryCode;
+	}
 
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
 
-    public String getPhoneCode() {
-        return phoneCode;
-    }
+	public String getPhoneCode() {
+		return phoneCode;
+	}
 
-    public void setPhoneCode(String phoneCode) {
-        this.phoneCode = phoneCode;
-    }
+	public void setPhoneCode(String phoneCode) {
+		this.phoneCode = phoneCode;
+	}
 }
