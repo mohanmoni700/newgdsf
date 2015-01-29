afc84eb1a4cd225d8fee35e9647670c53f5c1e7e
Santhosh Kattissery <santhosh.kattissery@compassitesinc.com>
2015-01-29 11:19:43 +0530

    Persist xml request responses

--- app/utils/XMLFileUtility.java
+++ app/utils/XMLFileUtility.java
@@ -1,19 +1,24 @@
 package utils;
 
-import com.thoughtworks.xstream.XStream;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.thoughtworks.xstream.XStream;
 
 /**
  * Created by user on 14-11-2014.
  */
 public class XMLFileUtility {
+	
+	static SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy-H:m:s");
 
 	public static void createXMLFile(Object object, String fileName) {
+		fileName = "xmls/" + fileName.split("\\.")[0] + dateFormat.format(new Date()) + ".xml";
 		Writer writer = null;
 		XStream xStream = new XStream();
 		try {
@@ -27,6 +32,8 @@ public class XMLFileUtility {
 	}
 
 	public static void createFile(String content, String filename) {
+		filename =  "xmls/" + filename.split("\\.")[0] + dateFormat.format(new Date()) + ".xml";
+		System.out.println(filename);
 		try {
 			FileWriter fileWriter = new FileWriter(new File(filename));
 			BufferedWriter writer = new BufferedWriter(fileWriter);
