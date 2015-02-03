a3d3f5451f4706b29ef13291f6e932548a11a24f
yaseen <yaseen.mulla@compassitesinc.com>
2015-02-03 10:29:11 +0530

    Redis cache time changed to 10 min

--- app/com/compassites/constants/CacheConstants.java
+++ app/com/compassites/constants/CacheConstants.java
@@ -4,5 +4,5 @@ package com.compassites.constants;
  * Created by Renu on 7/21/14.
  */
 public class CacheConstants {
-    public static final Integer CACHE_TIMEOUT_IN_SECS = 84600;
+    public static final Integer CACHE_TIMEOUT_IN_SECS = 600;
 }
