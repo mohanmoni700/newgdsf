1fae72df1b3c3c934e5576094d4c220344785e47
Yaseen <yaseen.mulla@compassitesinc.com>
2016-05-16 14:57:30 +0530

    added logger for 'JOCEnv' variable

--- app/utils/SystemUtility.java
+++ app/utils/SystemUtility.java
@@ -1,13 +1,19 @@
 package utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by yaseen on 12-05-2016.
  */
 public class SystemUtility {
 
+    static Logger logger = LoggerFactory.getLogger("gds");
+
+
     public static String getEnvironment() {
         String env = System.getenv("JOCEnv");
-
+        logger.debug("Loading Environment configuration for :" + env);
         return env;
     }
 }
