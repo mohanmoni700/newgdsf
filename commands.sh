99d8bfa5758df2cac510ed9f439989172b5e84d1
DilipJha <dilipkumar.jha@compassitesinc.com>
2017-06-12 16:24:37 +0530

    added automation files

---
+++ commands.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#export gdspid= `ps -ef | grep "jar gdsservice" | grep -v "grep" | awk '{print $2}'`
+#kill -9 $gdspid
+cd /opt/apps/flyhi/GDSServiceauto
+#chmod 664 application.properties
+#activator "run -Dhttp.port=9080 -Dconfig.resource=application.qa.conf"
