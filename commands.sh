e3c50cf5c6d999ccc83b309fc6d20e993df66963
DilipJha <dilipkumar.jha@compassitesinc.com>
2017-06-19 18:37:11 +0530

    updated code

--- commands.sh
+++ commands.sh
@@ -5,4 +5,5 @@ kill -9 $gdspid
 cd /opt/apps/flyhi/GDSService
 rm -rf RUNNING_PID
 cd /opt/apps/flyhi/GDSService/bin
-./gdsservice  -Dconfig.file=/opt/apps/flyhi/GDSService/bin/conf/application.qa.conf > /dev/null 2>&1&
+#./gdsservice  -Dconfig.file=/opt/apps/flyhi/GDSService/bin/conf/application.qa.conf > /dev/null 2>&1&
+./gdsservice -Dhttp.port=9080 -Dconfig.file=/opt/apps/flyhi/GDSService/bin/conf/application.qa.conf  > /dev/null 2>&1&
