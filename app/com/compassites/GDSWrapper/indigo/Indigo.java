fd6cfbb5ba3bb8c1ec72f0aed1e361f7d179d62d
sathishauit1 <satish.a@spekond.com>
2025-02-16 19:20:34 +0530

    inidigo session creation

---
+++ app/com/compassites/GDSWrapper/indigo/Indigo.java
@@ -0,0 +1,15 @@
+package com.compassites.GDSWrapper.indigo;
+
+import play.Play;
+
+public class Indigo {
+    public static final String DOMAIN_CODE;
+    public static final String AGENT_CODE;
+    public static final String AGENT_PASSWORD;
+    public static final Integer contractVersion=452;
+    static {
+        DOMAIN_CODE = Play.application().configuration().getString("indigo.domain.code");
+        AGENT_CODE = Play.application().configuration().getString("indigo.domain.agentCode");
+        AGENT_PASSWORD = Play.application().configuration().getString("indigo.domain.password");
+    }
+}
---
+++ app/com/compassites/GDSWrapper/indigo/SessionHandler.java
@@ -0,0 +1,68 @@
+package com.compassites.GDSWrapper.indigo;
+
+
+import com.navitaire.schemas.webservices.ISessionManager;
+import com.navitaire.schemas.webservices.LogonResponse;
+import com.navitaire.schemas.webservices.SessionManager;
+import com.navitaire.schemas.webservices.datacontracts.session.LogonRequestData;
+import com.navitaire.schemas.webservices.datacontracts.session.ObjectFactory;
+import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import com.navitaire.schemas.webservices.servicecontracts.sessionservice.LogonRequest;
+
+import javax.xml.bind.JAXBElement;
+import java.net.URL;
+import java.util.Date;
+
+@Service
+public class SessionHandler {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    private ISessionManager iSessionManager;
+
+    static Logger indigoLogger = LoggerFactory.getLogger("indigo");
+
+    public SessionHandler() {
+        try {
+            SessionManager sessionManager = new SessionManager();
+            iSessionManager = sessionManager.getBasicHttpBindingISessionManager();
+            ((javax.xml.ws.BindingProvider) iSessionManager)
+                    .getRequestContext()
+                    .put(javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "https://soapapir4y.test.6e.navitaire.com/SessionManager.svc");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public LogonResponse login() {
+        ObjectFactory factory = new ObjectFactory();
+        LogonRequest logonRequest = new LogonRequest();
+        LogonResponse logonResponse = null;
+        try {
+            LogonRequestData logonRequestData = new LogonRequestData();
+            logonRequestData.setDomainCode(factory.createLogonRequestDataDomainCode(Indigo.DOMAIN_CODE));
+            logonRequestData.setAgentName(factory.createLogonRequestDataAgentName(Indigo.AGENT_CODE));
+            logonRequestData.setPassword(factory.createLogonRequestDataPassword(Indigo.AGENT_PASSWORD));
+            logonRequest.setLogonRequestData(factory.createLogonRequestData(logonRequestData));
+            indigoLogger.debug("Indigo Session Req " + new Date() +" ---->" + new XStream().toXML(logonRequest));
+            SessionManager sessionManager = new SessionManager();
+            ISessionManager iSessionManager1 = sessionManager.getBasicHttpBindingISessionManager();
+            ((javax.xml.ws.BindingProvider) iSessionManager1)
+                    .getRequestContext()
+                    .put(javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "https://soapapir4y.test.6e.navitaire.com/SessionManager.svc");
+
+            logonResponse = iSessionManager1.logon(logonRequest, Indigo.contractVersion, false);
+            indigoLogger.debug("Indigo Session Response " + new Date() +" ---->" + logonResponse);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return logonResponse;
+    }
+
+}
