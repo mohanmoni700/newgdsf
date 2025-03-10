286aca5ec0e1291e371da6205630a5026e02cd06
sathishauit1 <satish.a@spekond.com>
2025-03-10 18:50:18 +0530

    indigo token saving

--- app/com/compassites/GDSWrapper/indigo/Indigo.java
+++ app/com/compassites/GDSWrapper/indigo/Indigo.java
@@ -7,6 +7,7 @@ public class Indigo {
     public static final String AGENT_CODE;
     public static final String AGENT_PASSWORD;
     public static final Integer contractVersion=452;
+    public static final Integer sessionValidity=30;
     static {
         DOMAIN_CODE = Play.application().configuration().getString("indigo.domain.code");
         AGENT_CODE = Play.application().configuration().getString("indigo.domain.agentCode");
