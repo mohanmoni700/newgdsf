843c24ca583dd7a44d3c77d30935a3c8c4c10f8a
Shilpa_A <a.shilpa@spekond.com>
2025-07-29 17:24:34 +0530

    Handled Changes and Cancellation with hyphen (#490)
    * Handled Changes and Cancellation with hyphen
    
    * Fnu and Lnu issue fix
    
    * optimized the code
    
    ---------
    
    Co-authored-by: Chetan-Spekond <Chetan-Spekond>

--- app/utils/AmadeusHelper.java
+++ app/utils/AmadeusHelper.java
@@ -152,16 +152,18 @@ public class AmadeusHelper {
         boolean changesProcessed = false;
         boolean cancellationsProcessed = false;
 
+        List<String> cancellationList = Arrays.asList("CANCELLATIONS", "CANCELLATION", "CANCELLATIONS-", "CANCELLATION-");
+        List<String> changeList = Arrays.asList("CHANGES", "CHANGE", "CHANGES-", "CHANGE-");
         while (index < fareRuleTextList.size()) {
             String trimmedValue = fareRuleTextList.get(index).getFreeText().toString().replaceAll("\\[|\\]", "").trim();
-            if (trimmedValue.equals("CHANGES") && !changesProcessed) {
+            if ((changeList.contains(trimmedValue) && !changesProcessed)) {
                 changesProcessed = true;
                 index++;
-                index = processRules(fareRuleTextList, index, changeMap, "CANCELLATIONS");
-            } else if (trimmedValue.equals("CANCELLATIONS") && !cancellationsProcessed) {
+                index = processRules(fareRuleTextList, index, changeMap, cancellationList);
+            } else if (cancellationList.contains(trimmedValue) && !cancellationsProcessed) {
                 cancellationsProcessed = true;
                 index++;
-                index = processRules(fareRuleTextList, index, cancelMap, "CHANGES");
+                index = processRules(fareRuleTextList, index, cancelMap, changeList);
             } else {
                 index++;
             }
@@ -408,7 +410,7 @@ public class AmadeusHelper {
         return hasYr && hasRefund;
     }
 
-    private static int processRules(List<FareCheckRulesReply.TariffInfo.FareRuleText> fareRuleTextList, int startIndex, Map<String, List<String>> rulesMap, String breakSection) {
+    private static int processRules(List<FareCheckRulesReply.TariffInfo.FareRuleText> fareRuleTextList, int startIndex, Map<String, List<String>> rulesMap, List<String> breakSectionList) {
 
         int index = startIndex;
         String currentCategory = null;
@@ -418,7 +420,7 @@ public class AmadeusHelper {
         while (index < fareRuleTextList.size()) {
             String text = fareRuleTextList.get(index).getFreeText().toString().replaceAll("\\[|\\]", "").trim();
 
-            if (text.equals(breakSection)) {
+            if (breakSectionList.contains(text)) {
                 if (currentCategory != null && !rules.isEmpty() && !rulesMap.containsKey(currentCategory)) {
                     rulesMap.put(currentCategory, new ArrayList<>(rules));
                 }
@@ -444,7 +446,7 @@ public class AmadeusHelper {
                 index++;
                 while (index < fareRuleTextList.size()) {
                     String nextText = fareRuleTextList.get(index).getFreeText().toString().replaceAll("\\[|\\]", "").trim();
-                    if (isSpecificCategory(nextText) || nextText.equals(breakSection)) {
+                    if (isSpecificCategory(nextText) || breakSectionList.contains(nextText)) {
                         break;
                     }
                     index++;
@@ -484,7 +486,11 @@ public class AmadeusHelper {
     }
 
     private static boolean isSpecificCategory(String line) {
-        return line.equalsIgnoreCase("ANY TIME") || line.equalsIgnoreCase("BEFORE DEPARTURE") || line.equalsIgnoreCase("AFTER DEPARTURE");
+        List<String> specificCategoryStringList = Arrays.asList("ANY TIME", "ANY TIME-", "BEFORE DEPARTURE", "BEFORE DEPARTURE-", "AFTER DEPARTURE", "AFTER DEPARTURE-");
+//        return line.equalsIgnoreCase("ANY TIME") || line.equalsIgnoreCase("BEFORE DEPARTURE") || line.equalsIgnoreCase("AFTER DEPARTURE")
+//        || line.equalsIgnoreCase("ANY TIME-") || line.equalsIgnoreCase("BEFORE DEPARTURE-") || line.equalsIgnoreCase("AFTER DEPARTURE-");
+//
+        return specificCategoryStringList.contains(line);
     }
 
 
